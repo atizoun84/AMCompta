27c8767ae6a54e977027aab70927f890501c2c8a
atizoun84 <atizoun@gmail.com>
2026-03-07 18:59:22 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const cacheName = 'amcompta-v1';
+const assets = [
+  './',
+  'index.html',
+  'logoapp.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie : Cache First, Fallback to Network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
